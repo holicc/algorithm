bd0035b9e380caba7ee83c3b47e3582dc424da9a
lu.longshan <lu.longshan@trs.com.cn>
2020-06-01 10:56:00 +0800

    AC 2

--- src/org/holicc/collection/Array.java
+++ src/org/holicc/collection/Array.java
@@ -2,12 +2,6 @@ package org.holicc.collection;
 
 public class Array {
 
-    public static void main(String[] args) {
-        Array array = new Array();
-        int i = array.maxSubArray_02(new int[]{-2, 1});
-        System.out.println(i);
-    }
-
     /**
      * 连续子数组的最大和
      * 时间复杂度 O(n^2)
@@ -70,4 +64,87 @@ public class Array {
         return f;
     }
 
+    /**
+     * 调整数组顺序使奇数位于偶数前面
+     * <p>
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(n)
+     * <p>
+     * https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+     */
+    public int[] exchange_01(int[] nums) {
+        int[] r = new int[nums.length];
+        int k = 0;
+        for (int num : nums) {
+            if ((num & 1) == 1) {
+                r[k++] = num;
+            }
+        }
+        //
+        for (int num : nums) {
+            if ((num & 1) == 0) {
+                r[k++] = num;
+            }
+        }
+        return r;
+    }
+
+    /**
+     * 调整数组顺序使奇数位于偶数前面
+     * <p>
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * <p>
+     * https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+     */
+    public int[] exchange_02(int[] nums) {
+        for (int i = 0, j = nums.length - 1; i < j; ) {
+            if ((nums[i] & 1) == 1) i++;
+            else if ((nums[j] & 1) == 0) j--;
+            else {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+        }
+        return nums;
+    }
+
+    /**
+     * 和为s的两个数字
+     * <p>
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * <p>
+     * https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+     */
+    public int[] twoSum_01(int[] nums, int target) {
+        int[] r = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) return new int[]{nums[i], nums[j]};
+            }
+        }
+        return r;
+    }
+
+    /**
+     * 和为s的两个数字
+     * <p>
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * <p>
+     * https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+     */
+    public int[] twoSum_02(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int s = nums[i] + nums[j];
+            if (s < target) i++;
+            else if (s > target) j--;
+            else return new int[]{nums[i], nums[j]};
+        }
+        return new int[0];
+    }
 }
