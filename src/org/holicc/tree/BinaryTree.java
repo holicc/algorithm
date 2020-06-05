a9b4b26fc6d090dca945cbb8cda4cf762d1302d9
lu.longshan <lu.longshan@trs.com.cn>
2020-06-05 15:29:04 +0800

    AC 1

--- src/org/holicc/tree/BinaryTree.java
+++ src/org/holicc/tree/BinaryTree.java
@@ -2,6 +2,11 @@ package org.holicc.tree;
 
 import org.holicc.model.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinaryTree {
 
     /**
@@ -24,4 +29,27 @@ public class BinaryTree {
         if (left != null && right != null) return root;
         return left == null ? right : left;
     }
+
+    /**
+     * 从上到下打印二叉树 II
+     * <p>
+     * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
 }
