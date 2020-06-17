218a9f3317b1c5efc97882614f178c7fdb74928a
lu.longshan <lu.longshan@trs.com.cn>
2020-06-17 11:00:28 +0800

    AC 2

--- src/org/holicc/collection/Array.java
+++ src/org/holicc/collection/Array.java
@@ -7,7 +7,8 @@ import java.util.List;
 public class Array {
 
     public static void main(String[] args) {
-        System.out.println(Math.pow(10, 3) - 1);
+        Array array = new Array();
+        System.out.println(array.constructArr_02(new int[]{1, 2, 3, 4, 5}));
     }
 
     /**
@@ -255,4 +256,105 @@ public class Array {
         return r;
     }
 
+    /**
+     * 最小的k个数
+     * 时间复杂度 O(n log(n))
+     * 空间复杂度 O(k)
+     * <p>
+     * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+     */
+    public int[] getLeastNumbers_01(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] r = new int[k];
+        System.arraycopy(arr, 0, r, 0, k);
+        return r;
+    }
+
+    /**
+     * 最小的k个数
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * <p>
+     * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+     */
+    public int[] getLeastNumbers_02(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // 最后一个参数表示我们要找的是下标为k-1的数
+        return quickSearch(arr, 0, arr.length - 1, k - 1);
+    }
+
+    private int[] quickSearch(int[] nums, int lo, int hi, int k) {
+        // 每快排切分1次，找到排序后下标为j的元素，如果j恰好等于k就返回j以及j左边所有的数；
+        int j = partition(nums, lo, hi);
+        if (j == k) {
+            return Arrays.copyOf(nums, j + 1);
+        }
+        // 否则根据下标j与k的大小关系来决定继续切分左段还是右段。
+        return j > k ? quickSearch(nums, lo, j - 1, k) : quickSearch(nums, j + 1, hi, k);
+    }
+
+    // 快排切分，返回下标j，使得比nums[j]小的数都在j的左边，比nums[j]大的数都在j的右边。
+    private int partition(int[] nums, int lo, int hi) {
+        int v = nums[lo];
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (++i <= hi && nums[i] < v) ;
+            while (--j >= lo && nums[j] > v) ;
+            if (i >= j) {
+                break;
+            }
+            int t = nums[j];
+            nums[j] = nums[i];
+            nums[i] = t;
+        }
+        nums[lo] = nums[j];
+        nums[j] = v;
+        return j;
+    }
+
+    /**
+     * 构建乘积数组
+     * 时间复杂度 O(n^2) 超时
+     * 空间复杂度 O(n)
+     * <p>
+     * https://leetcode-cn.com/problems/gou-jian-cheng-ji-shu-zu-lcof/
+     */
+    public int[] constructArr_01(int[] a) {
+        int[] r = new int[a.length];
+        //
+        for (int i = 0; i < r.length; i++) {
+            int t = a[i];
+            int mul = 1;
+            for (int j = 0; j < a.length; j++) {
+                if (j != i) mul *= a[j];
+            }
+            r[i] = mul;
+        }
+        return r;
+    }
+
+    /**
+     * 构建乘积数组
+     * <p>
+     * https://leetcode-cn.com/problems/gou-jian-cheng-ji-shu-zu-lcof/
+     */
+    public int[] constructArr_02(int[] a) {
+        if (a.length == 0) return new int[0];
+        int[] r = new int[a.length];
+        //
+        r[0] = 1;
+        //
+        for (int i = 1; i < r.length; i++) {
+            r[i] = r[i - 1] * a[i - 1];
+        }
+        //
+        int t = 1;
+        for (int i = a.length - 2; i >= 0; i--) {
+            t *= a[i + 1];
+            r[i] *= t;
+        }
+        return r;
+    }
 }
