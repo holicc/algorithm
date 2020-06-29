92294910d2327712c43bca510f9301d634f47ccd
lu.longshan <lu.longshan@trs.com.cn>
2020-06-29 15:37:59 +0800

    AC 2

--- src/org/holicc/collection/Array.java
+++ src/org/holicc/collection/Array.java
@@ -403,4 +403,43 @@ public class Array {
         }
         return -1;
     }
+
+    /**
+     * 顺时针打印矩阵
+     * <p>
+     * https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+     */
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) return new int[0];
+        int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+        int[] res = new int[(r + 1) * (b + 1)];
+        while (true) {
+            for (int i = l; i <= r; i++) res[x++] = matrix[t][i]; // left to right.
+            if (++t > b) break;
+            for (int i = t; i <= b; i++) res[x++] = matrix[i][r]; // top to bottom.
+            if (l > --r) break;
+            for (int i = r; i >= l; i--) res[x++] = matrix[b][i]; // right to left.
+            if (t > --b) break;
+            for (int i = b; i >= t; i--) res[x++] = matrix[i][l]; // bottom to top.
+            if (++l > r) break;
+        }
+        return res;
+    }
+
+    /**
+     * 0～n-1中缺失的数字
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * <p>
+     * https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
+     */
+    public int missingNumber(int[] nums) {
+        if (nums.length == 1 && nums[0] == 0) return 1;
+        int sum = (nums.length * (nums.length - 1)) / 2;
+        int real = 0;
+        for (int num : nums) {
+            real += num;
+        }
+        return sum - real;
+    }
 }
