5cb29d139eed12da58c7fbd3c6c3bbc489de7b13
lu.longshan <lu.longshan@trs.com.cn>
2020-07-03 11:32:00 +0800

    AC 1

--- src/org/holicc/collection/Array.java
+++ src/org/holicc/collection/Array.java
@@ -522,20 +522,7 @@ public class Array {
         return maxIndex;
     }
 
-    /**
-     * 青蛙跳台阶问题
-     * <p>
-     * https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
-     */
-    public int numWays(int n) {
-        int a = 1, b = 1, sum;
-        for (int i = 0; i < n; i++) {
-            sum = (a + b) % 1000000007;
-            a = b;
-            b = sum;
-        }
-        return a;
-    }
+
 
     /**
      * 二维数组中的查找
--- src/org/holicc/dp/DynamicProgramming.java
+++ src/org/holicc/dp/DynamicProgramming.java
@@ -30,4 +30,36 @@ public class DynamicProgramming {
         return p;
     }
 
+    /**
+     * 青蛙跳台阶问题
+     * <p>
+     * https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
+     */
+    public int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+    /**
+     * 斐波那契数列
+     * <p>
+     * 时间复杂度 O(n^2)
+     * 空间复杂度 O(n)
+     * <p>
+     * https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+     */
+    public int fib(int n) {
+        int a = 0, b = 1, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
 }
