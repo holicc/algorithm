79c64ab136c0ce501f166c6430061304be015a6e
lu.longshan <luls0728@zsmarter.com>
2020-08-10 10:10:11 +0800

    AC 1

--- src/org/holicc/dp/DynamicProgramming.java
+++ src/org/holicc/dp/DynamicProgramming.java
@@ -135,4 +135,22 @@ public class DynamicProgramming {
         }
         return profit;
     }
+
+    /**
+    * 剪绳子
+    * 时间复杂度 O(n)
+    * 空间复杂度 O(n2)
+    * <p>
+    * https://leetcode-cn.com/problems/jian-sheng-zi-lcof/
+    */
+    public int cuttingRope(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], (Math.max(j, dp[j])) * (Math.max(i - j, dp[i - j])));
+            }
+        }
+        return dp[n];
+    }
 }
