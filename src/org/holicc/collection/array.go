2b591614be1ddc6001afa740b51b22afeaf168a3
lu.longshan <luls0728@zsmarter.com>
2020-09-11 10:10:07 +0800

    >>>>

--- src/org/holicc/collection/array.go
+++ src/org/holicc/collection/array.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	kidsWithCandies([]int{2, 3, 1, 5, 3}, 3)
+	shuffle([]int{2, 5, 1, 3, 4, 7}, 3)
 }
 
 //数字序列中某一位的数字
@@ -61,6 +61,24 @@ func numIdenticalPairs(nums []int) int {
 			if nums[i] == nums[j] {
 				r++
 			}
+		}
+	}
+	return r
+}
+
+//https://leetcode-cn.com/problems/shuffle-the-array/
+//重新排列数组
+//给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+//请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+func shuffle(nums []int, n int) []int {
+	r := make([]int, len(nums))
+	for j, k, i := n, 0, 0; i < len(r); i++ {
+		if (i & 1) == 1 {
+			r[i] = nums[j]
+			j++
+		} else {
+			r[i] = nums[k]
+			k++
 		}
 	}
 	return r
