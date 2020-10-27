4bea347a8699040abde6e4d98a639ce460d6bb4a
lu.longshan <luls0728@zsmarter.com>
2020-10-27 10:38:24 +0800

    keep

--- src/org/holicc/bit/bit.go
+++ src/org/holicc/bit/bit.go
@@ -166,3 +166,28 @@ func bitCount(i int) int {
 	i = i + (i >> 16)
 	return i & 0x3f
 }
+
+// https://leetcode-cn.com/problems/reverse-bits/submissions/
+// 颠倒二进制位
+func reverseByte(b uint32, cache map[uint32]uint64) uint64 {
+	value, ok := cache[b]
+	if ok {
+		return value
+	}
+	value = (uint64(b) * 0x0202020202 & 0x010884422010) % 1023
+	cache[b] = value
+	return value
+}
+
+func reverseBits(num uint32) uint32 {
+	ret := uint64(0)
+	power := uint64(24)
+	var cache = map[uint32]uint64{}
+
+	for num != 0 {
+		ret += reverseByte(num&0xff, cache) << power
+		num = num >> 8
+		power -= 8
+	}
+	return uint32(ret)
+}
--- src/org/holicc/string/str.go
+++ src/org/holicc/string/str.go
@@ -452,3 +452,31 @@ func findTheDifference(s string, t string) byte {
 	}
 	return res
 }
+
+// https://leetcode-cn.com/problems/count-binary-substrings/
+// 计数二进制子串
+func countBinarySubstrings(s string) int {
+	counts := []int{}
+	ptr, n := 0, len(s)
+	for ptr < n {
+		c := s[ptr]
+		count := 0
+		for ptr < n && s[ptr] == c {
+			ptr++
+			count++
+		}
+		counts = append(counts, count)
+	}
+	ans := 0
+	for i := 1; i < len(counts); i++ {
+		ans += min(counts[i], counts[i-1])
+	}
+	return ans
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
