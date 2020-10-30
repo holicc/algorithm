a4672443559d1b35f7b926fc15d86f377b537a24
lu.longshan <luls0728@zsmarter.com>
2020-10-30 10:04:38 +0800

    keep

--- src/org/holicc/collection/Array.java
+++ src/org/holicc/collection/Array.java
@@ -620,5 +620,29 @@ public class Array {
         }
         return k == 0;
     }
+
+    // https://leetcode-cn.com/problems/last-stone-weight/
+    // 最后一块石头的重量
+    public int lastStoneWeight(int[] stones) {
+        int len = stones.length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(len, (o1, o2) -> -o1 + o2);
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+
+        while (maxHeap.size() >= 2) {
+            Integer head1 = maxHeap.poll();
+            Integer head2 = maxHeap.poll();
+        if (head1.equals(head2)) {
+            continue;
+        }
+            maxHeap.offer(head1 - head2);
+        }
+
+        if (maxHeap.isEmpty()) {
+            return 0;
+        }
+        return maxHeap.poll();
+    }
 }
 
