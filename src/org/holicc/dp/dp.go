2a859bdf2207c898f03ccf0d29ba77771bf7b0be
lu.longshan <luls0728@zsmarter.com>
2020-11-09 11:10:50 +0800

    keep

--- src/org/holicc/dp/dp.go
+++ src/org/holicc/dp/dp.go
@@ -69,3 +69,18 @@ func cuttingRope(n int) int {
 		return pow3N(n/3-m) * (m + 1) * 2 % 1000000007
 	}
 }
+
+// https://leetcode-cn.com/problems/chuan-di-xin-xi/
+// 传递信息
+func numWays(n int, relation [][]int, k int) int {
+	d := make([]int, n)
+	d[0] = 1
+	for i := 0; i < k; i++ {
+		t := make([]int, n)
+		for _, j := range relation {
+			t[j[1]] += d[j[0]]
+		}
+		d = t
+	}
+	return d[n-1]
+}
