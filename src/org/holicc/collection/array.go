5240720b16d78eb020ae6ba5ac934abbb10fb9de
lu.longshan <luls0728@zsmarter.com>
2020-12-07 09:53:44 +0800

    keep

--- src/org/holicc/collection/array.go
+++ src/org/holicc/collection/array.go
@@ -902,3 +902,15 @@ func findDisappearedNumbers(nums []int) []int {
 	}
 	return []int{}
 }
+
+// https://leetcode-cn.com/problems/largest-perimeter-triangle/
+// 三角形的最大周长
+func largestPerimeter(a []int) int {
+	sort.Ints(a)
+	for i := len(a) - 1; i >= 2; i-- {
+		if a[i-2]+a[i-1] > a[i] {
+			return a[i-2] + a[i-1] + a[i]
+		}
+	}
+	return 0
+}
