13a2306903510c4496b5b0ddf793e7949a39df6c
lulongshan <ztzu_lls@zmaster.com>
2020-12-15 11:05:33 +0800

    keep

--- src/org/holicc/collection/map_.go
+++ src/org/holicc/collection/map_.go
@@ -1,5 +1,7 @@
 package main
 
+import "unicode"
+
 // https://leetcode-cn.com/problems/find-lucky-integer-in-an-array/
 // 找出数组中的幸运数
 func findLucky(arr []int) int {
@@ -61,4 +63,48 @@ func romanToInt(s string) int {
 		pre = cur
 	}
 	return r
+}
+
+// https://leetcode-cn.com/problems/shortest-completing-word/
+// 最短补全词
+func shortestCompletingWord(licensePlate string, words []string) string {
+	lengthWords := len(words)
+	min := 16
+	minWord := ""
+
+	//依次遍历取出words里的每个单词
+	for i := 0; i < lengthWords; i++ {
+		wordLength := len(words[i])
+		list := make([]int, 26)
+
+		//如果该单词比当前最短完整词的长度长则舍去
+		if wordLength < min {
+
+			//依次将每个单词转换到长度为26的哈希表里(切片做哈希表)
+			for j := 0; j < wordLength; j++ {
+				list[words[i][j]-'a']++
+			}
+
+			//依次遍历比较牌照中的字母是否都在该单词里
+			if compare(list, licensePlate) {
+
+				//如果存在则更新最短完整词和最短长度
+				min = wordLength
+				minWord = words[i]
+			}
+		}
+	}
+	return minWord
+}
+
+func compare(list []int, licensePlate string) bool {
+	for i := 0; i < len(licensePlate); i++ {
+		if letter := licensePlate[i]; unicode.IsLetter(rune(letter)) {
+			if list[(letter|32)-'a'] <= 0 {
+				return false
+			}
+			list[(letter|32)-'a']--
+		}
+	}
+	return true
 }
