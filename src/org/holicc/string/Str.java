578eac4f71ae95311379f5ac1a07d70682068fbc
lulongshan <ztzu_lls@zmaster.com>
2021-01-18 14:16:09 +0800

    我是傻逼

--- src/org/holicc/string/Str.java
+++ src/org/holicc/string/Str.java
@@ -111,4 +111,25 @@ public class Str {
         return new String(a);
     }
 
+
+    /**
+     * 仅仅反转字母
+     * <p>
+     * https://leetcode-cn.com/problems/reverse-only-letters/
+     */
+    public String reverseOnlyLetters(String S) {
+        Stack<Character> letters = new Stack();
+        for (char c : S.toCharArray())
+            if (Character.isLetter(c))
+                letters.push(c);
+
+        StringBuilder ans = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            if (Character.isLetter(c))
+                ans.append(letters.pop());
+            else
+                ans.append(c);
+        }
+        return ans.toString();
+    }
 }
